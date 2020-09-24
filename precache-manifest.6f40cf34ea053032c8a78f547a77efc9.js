3e021e51d7199a8c3d6c4c102be3a3d18b5c79aa
Seth White <sethewhite@me.com>
2020-09-24 00:45:43 -0400

    Updates

---
+++ precache-manifest.6f40cf34ea053032c8a78f547a77efc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4f39ba42973aabd08554ab9d1e903c",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "1e29eeb503ddaae7d111",
+    "url": "/my-portfolio/static/css/main.a8bb6816.chunk.css"
+  },
+  {
+    "revision": "07a8eb26ae4a8a50a92f",
+    "url": "/my-portfolio/static/js/2.09835eb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.09835eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e29eeb503ddaae7d111",
+    "url": "/my-portfolio/static/js/main.2419d466.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
